c4e3c0b71ccd738ab6a48870dacba3cd0555403e
Morris Ifarajimi <morris@iMac-di-Morris.station>
2023-11-02 18:47:57 +0100

    Esercizio 1

---
+++ main.js
@@ -0,0 +1,56 @@
+/* ESERCIZIO 1 */
+
+let name = `Ciao sono Morris, ho`
+let age = 37;
+let subject = `anni e sto studiando JavaScript`
+let FaveTeacher= `con il mio insegnante preferito Valario Vacca!`
+let Tot = `${name} ${age} ${subject} ${FaveTeacher}`
+
+console.log(Tot);
+
+/* ESERCIZIO 2 */
+
+let number1 = 10;
+let number2 = 15;
+let number3 = 20;
+let number4 = 25;
+let number5 = 30;
+let sum = number1 + number2 + number3 + number4 + number5
+console.log(sum)
+
+let media = sum/5;
+console.log(media)
+
+/* ESERCIZIO 3 */
+
+let ac = 2023;
+let anno_nasciata = 1986;
+let age1 = ac - anno_nasciata;
+console.log(age1)
+
+let cento = 100 - age1;
+console.log (cento)
+
+let frase = `Hai ${age1} anni e ti mancano ${cento} anni per compierne 100`
+console.log (frase)
+
+
+/* ESERCIZIO 4 */
+
+let gatti_tot = 44;
+let gatti_fila = 6;
+let file = gatti_tot/gatti_fila;
+let file_piene_intere = Math.floor(file)
+console.log(file_piene_intere);
+
+let gatti_gia_in_fila = (gatti_tot-(file_piene_intere * gatti_fila));
+/* let gatti_gia_in_fila = gatti_fila % gatti_fila */
+console.log(gatti_gia_in_fila);
+
+let gatti_mancanti_fila_da_riempire = gatti_fila - gatti_gia_in_fila;
+console.log(gatti_mancanti_fila_da_riempire);
+
+let frase2 = `Ci sono ${file_piene_intere} file di gatti e ne mancano ${gatti_mancanti_fila_da_riempire} per una nuova fila, con un avanzo di ${gatti_gia_in_fila}`
+console.log(frase2)
+
+
